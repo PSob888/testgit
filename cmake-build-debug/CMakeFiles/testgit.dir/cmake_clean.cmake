a1e1a2124684b24ac54850ec8c65710e366b6141
PSob888 <79452273+PSob888@users.noreply.github.com>
2021-02-22 12:54:50 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/testgit.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/testgit.dir/main.c.obj"
+  "libtestgit.dll.a"
+  "testgit.exe"
+  "testgit.exe.manifest"
+  "testgit.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/testgit.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
